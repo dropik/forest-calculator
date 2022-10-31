ef3f07c02fdae2ad5d300b3fc28a405ed1cadad5
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-31 23:55:09 +0100

    Implemented simple bezier shape drawing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,51 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('drawRoot')
   private canvas?: ElementRef<HTMLCanvasElement>;
   private context?: CanvasRenderingContext2D;
+  private startedDrawing = false;
+  private prevPointCoords: { x: number, y: number } = { x: 0, y: 0 };
+  private firstPointCoords = this.prevPointCoords;
 
   ngAfterViewInit(): void {
-    this.context = this.canvas?.nativeElement.getContext('2d')!;
+    if (this.canvas) {
+      this.canvas.nativeElement.width = document.body.clientWidth;
+      this.canvas.nativeElement.height = document.body.clientHeight;
+      this.context = this.canvas.nativeElement.getContext('2d')!;
+    }
+  }
+
+  public clickHandler(e: MouseEvent): void {
+    if (!this.startedDrawing) {
+      this.startedDrawing = true;
+      this.startDrawing(e);
+    } else {
+      this.updatePath(e);
+    }
+  }
+
+  public startDrawing(e: MouseEvent): void {
+    this.prevPointCoords = { x: e.x, y: e.y };
+    this.firstPointCoords = this.prevPointCoords;
+  }
+
+  public stopDrawing(): void {
+    if (this.context) {
+      this.context.beginPath();
+      this.context.moveTo(this.prevPointCoords.x, this.prevPointCoords.y);
+      this.context.bezierCurveTo(this.prevPointCoords.x, this.prevPointCoords.y, this.firstPointCoords.x, this.firstPointCoords.y, this.firstPointCoords.x, this.firstPointCoords.y);
+      this.context.stroke();
+      this.context.closePath();
+    }
+    this.startedDrawing = false;
+  }
+
+  public updatePath(e: MouseEvent): void {
+    if (this.context) {
+      this.context.beginPath();
+      this.context.moveTo(this.prevPointCoords.x, this.prevPointCoords.y);
+      this.context.bezierCurveTo(this.prevPointCoords.x, this.prevPointCoords.y, e.x, e.y, e.x, e.y);
+      this.prevPointCoords = { x: e.x, y: e.y };
+      this.context.stroke();
+      this.context.closePath();
+    }
   }
 }
